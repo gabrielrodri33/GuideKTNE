9a2986305ab435e17bf9cf75e6d64f004bb59f18
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-12-27 19:53:30 -0300

    Lógica finalizada

--- scripts/diagrama.py
+++ scripts/diagrama.py
@@ -1,6 +1,25 @@
 import os
 from itertools import permutations
 
+'''
+Vermelho / v OK
+Vermelho - Led / vl OK
+Vermelho - Estrela / ve OK
+Vermelho - Led - Estrela / vle OK
+Vermelho - Azul / va OK
+Vermelho - Azul - Led / val OK
+Vermelho - Azul - Led - Estrela / vale 
+
+Azul / a OK
+Azul - Led / al OK
+Azul - Estrela / ae OK
+Azul - Estrela - Led / ael OK
+
+Led / l OK
+Estrela / e OK
+Led - Estrela / le OK
+'''
+
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -35,14 +54,18 @@ def separador(n, cor):
 def answer(option):
     cores = {
         1: {'verde': '\033[32m', 'limpa' : '\033[0m'},
-        2: {'vermelho': '\033[31m', 'limpa': '\033[0m'}
+        2: {'vermelho': '\033[31m', 'limpa': '\033[0m'},
+        3: {'amarelo': '\033[33m', 'limpa': '\033[0m'}
     }
 
     if option == 1:
         mensagem = f'{cores[1]["verde"]}Corta!{cores[1]["limpa"]}'
     
-    else:
+    elif option == 2:
         mensagem = f'{cores[2]["vermelho"]}Não Corta!{cores[2]["limpa"]}'
+
+    else:
+        mensagem = f'{cores[3]["amarelo"]}Número Igual!{cores[3]["limpa"]}'
 
     return print(mensagem)
 
@@ -51,25 +74,18 @@ def validation(option):
     match option:
         case 1:
             while not status:
-                try:
-                    
-                    option = input("Contém entrada paralela? [S/N] ").upper().strip()
-                    
-                    if option and option[0] in ["S", "N"]:
-                        status = True
-
-                    else:
-                        separador("Entrada inválida!", 7)
-                        print('Digite "S" ou "N"')
-                        separador(28, 1)
+                option = input("Contém entrada paralela? [S/N] ").upper().strip()
                 
-                except ValueError as e:
-                    print(f"Error: {e}")
-        
+                if option and option[0] in ["S", "N"]:
+                    status = True
+
+                else:
+                    separador("Entrada inválida!", 7)
+                    print('Digite "S" ou "N"')
+                    separador(28, 1)
+
         case 2:
-            while not status:
-                try:
-                    
+            while not status:  
                     option = input("Contém 2 ou mais pilhas? [S/N] ").upper().strip()
                     
                     if option and option[0] in ["S", "N"]:
@@ -79,14 +95,9 @@ def validation(option):
                         separador("Entrada inválida!", 7)
                         print('Digite "S" ou "N"')
                         separador(28, 1)
-                
-                except ValueError as e:
-                    print(f"Error: {e}")
-        
+            
         case 3:
-            while not status:
-                try:
-                    
+            while not status: 
                     option = input("O último dígito é par? [S/N] ").upper().strip()
                     
                     if option and option[0] in ["S", "N"]:
@@ -97,9 +108,6 @@ def validation(option):
                         print('Digite "S" ou "N"')
                         separador(28, 1)
                 
-                except ValueError as e:
-                    print(f"Error: {e}")
-    
     return option
 
 def primary_lists():
@@ -135,45 +143,15 @@ def primary_lists():
 
     return red, blue, star, led
 
-'''
-Vermelho / v 
-Vermelho - Led / vl 
-Vermelho - Estrela / ve
-Vermelho - Led - Estrela / vle
-Vermelho - Azul / va
-Vermelho - Azul - Led / val
-Vermelho - Azul - Led - Estrela / vale
-
-Azul / a
-Azul - Led / al
-Azul - Estrela / ae
-Azul - Estrela - Led / ael
-
-Led / l
-Estrela / e
-Led - Estrela / le
-'''
-
 def secondary_lists(red, blue, star, led):
     no_cut = cut = dynamic_list = 0
 
-    red_blue = [red[2], red[3], red[4], red[5]]
-    red_star = [red[1], red[3], red[5], red[7]]
-    red_led = [red[4], red[5], red[6], red[7]]
-    red_blue_led = [red[4], red[5]]
-    red_blue_star = [red[4], "N"]
-    red_blue_star_led = ["N"]
-    red_led_star = ["N", red[7]]
-
-    blue_led = [blue[1], blue[3], blue[5], blue[7]]
-    blue_star = [blue[4], blue[5], blue[6], blue[7]]
-    blue_star_led = [blue[5], blue[7]]
-
-    led_star = [led[4], led[5], led[6], led[7]]
-
     word = search_word()
 
-    dynamic_list = red
+    if word == "S":
+        return word
+
+    dynamic_list, type_wire = dynamyc(red, blue, star, led, word)
 
     for i in dynamic_list:
         if i == "N":
@@ -183,16 +161,19 @@ def secondary_lists(red, blue, star, led):
             cut += 1
 
     if no_cut > cut:
+        print(type_wire)
         answer(2)
 
     elif no_cut == cut: 
-        answer(2)
+        print(type_wire)
+        answer(3)
 
     else:
+        print(type_wire)
         answer(1)
 
 def search_word():
-    words = ["V", "VL", "VE", "VLE", "VA", "VAL", "VALE", "A", "AL", "AE", "AEL", "L", "E", "LE"]
+    words = ["V", "VL", "VE", "VLE", "VA", "VAL", "VALE", "A", "AL", "AE", "AEL", "L", "E", "LE", "S"]
 
     while True:
         separador(28, 1)
@@ -212,13 +193,73 @@ def search_word():
 
     return word
 
+def dynamyc(red, blue, star, led, word):
+    red_blue = [red[2], red[3], red[4], red[5]]
+    red_star = [red[1], red[3], red[5], red[7]]
+    red_led = [red[4], red[5], red[6], red[7]]
+    red_blue_led = [red[4], red[5]]
+    red_blue_star = [red[4], "N"]
+    red_blue_star_led = ["N"]
+    red_led_star = ["N", red[7]]
+
+    blue_led = [blue[1], blue[3], blue[5], blue[7]]
+    blue_star = [blue[4], blue[5], blue[6], blue[7]]
+    blue_star_led = [blue[5], blue[7]]
+
+    led_star = [led[4], led[5], led[6], led[7]]
+
+    match word:
+        case "V":
+            return red, "Fio vermelho!"
+        
+        case "VL":
+            return red_led, "Fio vermelho com led!"
+
+        case "VE":
+            return red_star, "Fio vermelho com estrela!"
+
+        case "VLE":
+            return red_led_star, "Fio vermelho com led e estrela!"
+
+        case "VA":
+            return red_blue, "Fio vermelho e azul!"
+
+        case "VAL":
+            return red_blue_led, "Fio vermelho com azul e led!"
+
+        case "VALE":
+            return red_blue_star_led, "Fio com tudo!"
+
+        case "A":
+            return blue, "Fio azul!"
+
+        case "AL":
+            return blue_led, "Fio azul com led!"
+
+        case "AE":
+            return blue_star, "Fio azul com estrela!"
+
+        case "AEL":
+            return blue_star_led, "Fio azul com estrela e led!"
+        
+        case "L":
+            return led, "Fio branco com led!"
+    
+        case "E":
+            return star, "Fio branco com estrela!"
+        
+        case "LE":
+            return led_star, "Fio branco com estrela e led!"
+        
+        case _:
+            return "S"
+
 def principal():
     count = 0
 
     red, blue, star, led = primary_lists()
-    while count < 6:
-        secondary_lists(red, blue, star, led)
-        count += 1
+    while count != "S":
+        count = secondary_lists(red, blue, star, led)
 
 #Programa principal
 principal()
